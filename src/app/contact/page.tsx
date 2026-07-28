d4f17d83dd32b8bc119daf75dabeb929b60cc92b
Arghajit Singha <arghajitsingha@Arghajits-MacBook-Air.local>
2026-07-29 01:49:45 +0530

    fix(KAN-80): Move PropertyContactForm to /contact page
    - Remove PropertyContactForm from /properties page
    - Create new /contact page with PropertyContactForm
    - All 28 unit tests still passing
    - Build succeeds
    - Fixes incorrect page placement from KAN-29
    
    Closes KAN-80

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { PropertyContactForm } from "@/components/properties/PropertyContactForm";
+
+export default function ContactPage() {
+  return (
+    <main className="flex min-h-screen flex-col bg-zinc-950 text-zinc-100">
+      <PropertyContactForm />
+    </main>
+  );
+}
